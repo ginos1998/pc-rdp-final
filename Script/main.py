da7399bea8d0d746e1f4b1d5c6852b95a347fefe
AgusMaff <102746223+AgusMaff@users.noreply.github.com>
2024-02-17 14:06:57 -0300

    Update main.py

--- Script/main.py
+++ Script/main.py
@@ -1,6 +1,34 @@
-from Modules.invariantes import get_invariants
+import re
+from Modules.invariantes import get_invariants, check_invT
 from Modules.transiciones import get_transitions
 
-get_transitions()
-get_invariants()
+# Definición de los invariantes de transición
+invariantes = [
+        [1, 2, 4, 6, 8],
+        [1, 3, 5, 7, 8],
+        [9, 10, 11, 12]
+    ]
 
+reggex = r"(T1(?:T2T4T6|T3T5T7)T8)|(T9T10T11T12)"
+
+#Direccion del archivo que quiero leer 
+path = '/home/agustin/Desktop/Concurrente/pc-rdp-final/log.txt'
+
+# Diccionario para contar las ocurrencias de cada transición
+transiciones_contador = {'T' + str(i): 0 for i in range(1, 13)}
+
+# Contador de ejecuciones para cada invariante
+invariantes_contador = [0 for _ in invariantes]
+
+# Lista para almacenar los tiempos de disparo de las transiciones
+tiempos = []
+
+# Expresión regular para identificar las líneas que indican una transición disparada
+patron_transicion = re.compile(r'(\d+) Transition (T\d+) fired')
+
+#Cadena con las transiciones para posteriormente verificar los invariantes de plaza
+secuencia = ""
+
+secuencia = get_transitions(path, patron_transicion, transiciones_contador, tiempos, secuencia)
+get_invariants(invariantes_contador, transiciones_contador, invariantes)
+check_invT(secuencia, reggex)
